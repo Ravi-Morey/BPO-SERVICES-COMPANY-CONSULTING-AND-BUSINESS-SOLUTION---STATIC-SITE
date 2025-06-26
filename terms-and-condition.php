e648b487a9b3c109b5fdaa8a7eacd27abd4077de
Ravi Morey <124388301+Ravi-Morey@users.noreply.github.com>
2025-06-26 16:14:18 +0530

    Required for legal compliance if collecting data

---
+++ terms-and-condition.php
@@ -0,0 +1,135 @@
+<?php
+$pageTitle = "Terms and Condition - Quanteq Solutions Pvt Ltd.";
+$pageDescription = "Learn more about Quanteq Solutions Pvt Ltd., our mission, vision, and the team behind our success.";
+$pageKeywords = "Quanteq Solutions, about us, our team, mission, vision";
+include 'template/header.php';
+?>
+
+<section class="hero-wrap hero-wrap-2" style="background-image: url('images/bg_1.jpg');">
+      <div class="overlay"></div>
+      <div class="container">
+        <div class="row no-gutters slider-text align-items-center justify-content-center">
+          <div class="col-md-9 ftco-animate text-center">
+            <h1 class="mb-2 bread">Terms and Condition</h1>
+            <p class="breadcrumbs"><span class="mr-2"><a href="index.php">Home <i class="ion-ios-arrow-forward"></i></a></span> <span>Terms and Condition <i class="ion-ios-arrow-forward"></i></span></p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+
+    <section class="ftco-section ftco-animate">
+            <div class="container" >
+                <div class="row d-flex">
+                    
+                    <div class="col-md-12 wrap-about pr-md-4 ftco-animate">
+                        <!-- <div class="img " style="background-image: url(images/image_2.jpg);" ></div> -->
+            <p class="mb-4">Welcome to Quanteq Solutions. By accessing or using our website, you agree to be bound by these Terms and Conditions. Please read them carefully before using our services.</p>
+            <h3 style="font-size: 28px;">Acceptance of Terms
+</h3>
+                        <!-- <p>We offer a range of services designed to address the unique challenges of today’s business landscape: </p> -->
+                        <div class="links">
+                        <ul class="mb-4 pl-3" >
+                                            <!-- list 1 -->
+                                            <li >
+                                                <!-- icon -->
+                                                
+                                                
+                                                <!-- content -->
+                                                <div class="content"> 
+                                                  <!-- <h3>We Respect Your Privacy</h3> -->
+                                                </div>
+                                                
+                                                <p>By accessing this website, you agree to comply with these terms. If you do not agree with any part of the terms, you must not use our website.
+ </p>
+                                                
+                                            </li>
+                                            
+                                            
+
+                                            
+                                        </ul>
+                                    </div>
+                        
+                        <h3 style="font-size: 28px;">Use of Our Services</h3>
+                        
+                        <div class="links">
+                        <ul class="mb-4 pl-3" >
+                                            <!-- list 1 -->
+                                            <li >
+                                                <!-- icon -->
+                                                
+                                                
+                                                <!-- content -->
+                                                <!-- <div class="content"> <h3>User-Centered Design</h3></div> -->
+                                                
+                                                <p>You agree to use our website only for lawful purposes. You must not use the site to conduct any activity that is fraudulent, offensive, or otherwise harmful to us or other users. You are responsible for maintaining the confidentiality of your account information.</p>
+                                                
+                                            </li>
+                                            
+                                        </ul>
+                                    </div>
+
+                                     <h3 style="font-size: 28px;">Intellectual Property</h3>
+                        <!-- <p>By integrating cutting-edge technology with a focus on human experience, Quanteq Solutions helps businesses achieve: </p> -->
+                        <div class="links">
+                        <ul class="mb-4 pl-3" >
+                                            <!-- list 1 -->
+                                            <li >
+                                                
+                                                <p>All content on this website, including text, graphics, logos, and software, is the property of Quanteq Solutions or its content suppliers and is protected by intellectual property laws. Unauthorized use of this content is prohibited.</p>
+                                                
+                                            </li>
+      </ul>
+
+                                        <h3 style="font-size: 28px;">Privacy Policy</h3>
+                        <!-- <p>By integrating cutting-edge technology with a focus on human experience, Quanteq Solutions helps businesses achieve: </p> -->
+                        <div class="links">
+                        <ul class="mb-4 pl-3" >
+                                            <!-- list 1 -->
+                                            <li >
+                                                
+                                                <p>Your privacy is important to us. Our<strong><a href="privacy-policy.php"> Privacy Policy </a></strong>explains how we collect, use, and protect your information. By using our site, you consent to the data practices described in our Privacy Policy.</p>
+                                                
+                                            </li>
+                                            
+                                            
+                                        </ul>
+                                        <h3 style="font-size: 28px;">Limitation of Liability</h3>
+                        <!-- <p>By integrating cutting-edge technology with a focus on human experience, Quanteq Solutions helps businesses achieve: </p> -->
+                        <div class="links">
+                        <ul class="mb-4 pl-3" >
+                                            <!-- list 1 -->
+                                            <li >
+                                                
+                                                <p>Quanteq Solutions will not be liable for any damages arising from the use of this site. This includes direct, indirect, incidental, or consequential damages.</p>
+                                                
+                                            </li>
+                                            
+                                            
+                                        </ul>
+                                        <h3 style="font-size: 28px;">Modifications to Terms</h3>
+                        <!-- <p>By integrating cutting-edge technology with a focus on human experience, Quanteq Solutions helps businesses achieve: </p> -->
+                        <div class="links">
+                        <ul class="mb-4 pl-3" >
+                                            <!-- list 1 -->
+                                            <li >
+                                                
+                                                <p>We reserve the right to update these terms at any time without prior notice. Continued use of the site after any changes indicates your acceptance of the new terms.</p>
+                                                
+                                            </li>
+                                            
+                                            
+                                        </ul>
+                                        <h3 style="font-size: 28px;">Governing Law</h3>
+                        <p>These terms are governed by and construed in accordance with the laws of Indian Jurisdiction, without regard to its conflict of law principles. </p> 
+                        <p>For any questions regarding these terms, please contact us at <strong><a href="mailto:hr@quanteqsolutions.com" target="__black">hr@quanteqsolutions.com</a></strong></p>
+                                    </div>
+                                  </div>
+                                </div>
+                              </div>
+                            </section>
+
+
+
+    <?php include('template/footer.php'); ?>

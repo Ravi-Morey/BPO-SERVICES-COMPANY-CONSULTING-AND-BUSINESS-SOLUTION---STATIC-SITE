223955f0c00d70aa9c3b6c554a396e95c6d67e93
Ravi Morey <124388301+Ravi-Morey@users.noreply.github.com>
2025-06-26 15:50:46 +0530

    Useful short information

---
+++ footer.php
@@ -0,0 +1,460 @@
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Footer - Quanteq Soluton Pvt Ltd.</title>
+  <meta name="description" content="">
+
+  <link rel="canonical" href="https://quanteqsolutions.com/">
+	<link href="https://fonts.googleapis.com/css?family=Nunito+Sans:300,400,600,700,800,900&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    
+    <link rel="stylesheet" href="css/open-iconic-bootstrap.min.css">
+    <link rel="stylesheet" href="css/animate.css">
+    
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="css/magnific-popup.css">
+
+    <link rel="stylesheet" href="css/aos.css">
+
+    <link rel="stylesheet" href="css/ionicons.min.css">
+    
+    <link rel="stylesheet" href="css/flaticon.css">
+    <link rel="stylesheet" href="css/icomoon.css">
+    <link rel="stylesheet" href="css/style.css">
+    <style type="text/css">
+      
+      ul {
+    margin: 0px;
+    padding: 0px;
+}
+.footer-section {
+  background: #fffff;
+  position: relative;
+}
+.footer-cta {
+  border-bottom: 1px solid #299894;
+
+}
+.single-cta{
+  border: 1px solid;
+  border-radius: 50px;
+  background:#299894 ;
+  text-align: center;
+  padding-top: 5px;
+  padding-bottom: 5px;
+  margin-bottom: 5px;
+
+
+}
+.single-cta:hover{
+  background:#d1bf6c ;
+}
+.single-cta i {
+  color: #000;
+  font-size: 30px;
+  float: left;
+  margin-top: 8px;
+
+}
+.cta-text {
+  padding-left: 0px;
+  display: inline-block;
+
+}
+.cta-text h4 {
+  color: #fff;
+  font-size: 20px;
+  font-weight: 600;
+  margin-bottom: 2px;
+}
+.cta-text span {
+  color: #fff;
+  font-size: 15px;
+}
+.footer-content {
+  position: relative;
+  z-index: 2;
+}
+.footer-pattern img {
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 330px;
+  background-size: cover;
+  background-position: 100% 100%;
+}
+.footer-logo {
+  margin-bottom: 30px;
+}
+.footer-logo img {
+    max-width: 200px;
+}
+.footer-text p {
+  margin-bottom: 14px;
+  font-size: 14px;
+      color: #7e7e7e;
+  line-height: 28px;
+}
+.footer-social-icon span {
+  color: #000;
+  display: block;
+  font-size: 20px;
+  font-weight: 700;
+  font-family: 'Poppins', sans-serif;
+  margin-bottom: 20px;
+}
+.footer-social-icon a {
+  color: #000;
+  font-size: 16px;
+  margin-right: 15px;
+}
+.footer-social-icon i {
+  height: 40px;
+  width: 40px;
+  text-align: center;
+  line-height: 38px;
+  border-radius: 50%;
+}
+.facebook-bg{
+  background: #3B5998;
+}
+.twitter-bg{
+  background: #55ACEE;
+}
+.google-bg{
+  background: #DD4B39;
+}
+.footer-widget-heading h3 {
+  color: #000;
+  font-size: 20px;
+  font-weight: 700;
+  margin-bottom: 40px;
+  position: relative;
+}
+.footer-widget-heading h3::before {
+  content: "";
+  position: absolute;
+  left: 0;
+  bottom: -15px;
+  height: 2px;
+  width: 50px;
+  background: #299894;
+}
+.footer-widget ul li {
+  display: inline-block;
+  float: left;
+  width: 100%;
+  margin-bottom: 12px;
+}
+.footer-widget ul li a:hover{
+  color: #d1bf6c;
+}
+.footer-widget ul li a {
+  color: #299894;
+  text-transform: capitalize;
+}
+.subscribe-form {
+  position: relative;
+  overflow: hidden;
+}
+.subscribe-form input {
+  width: 100%;
+  padding: 14px 28px;
+  background: #f5f5f5;
+  border: 1px solid #2E2E2E;
+  color: #000;
+
+}
+.subscribe-form button {
+    position: absolute;
+    right: 0;
+    background: #299894;
+    padding: 13px 20px;
+    border: 1px solid #000;
+    top: 0;
+}
+.subscribe-form button i {
+  color: #000;
+  font-size: 22px;
+  transform: rotate(-6deg);
+}
+.copyright-area{
+  background: #01605c;
+  padding: 25px 0;
+}
+.copyright-text p {
+  margin: 0;
+  font-size: 14px;
+  color: #fff;
+}
+.copyright-text p a{
+  color: #ff5e14;
+}
+.footer-menu li {
+  display: inline-block;
+  margin-left: 20px;
+}
+.footer-menu li:hover a{
+  color: #d1bf6c;
+}
+.footer-menu li a {
+  font-size: 14px;
+  color: #fff;
+}
+    </style>
+</head>
+<body>
+
+  <footer class="footer-section">
+        <div class="container">
+            <div class="footer-cta pt-5 pb-5">
+                <div class="row">
+                    <div class="col-xl-4 col-md-4 mb-30">
+                        <div class="single-cta">
+                            <!-- <i class="fas fa-map-marker-alt"></i> -->
+                            <div class="cta-text">
+                                <h4>Find us</h4>
+                                <span> Gurgaon 122002, Haryana</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-4 col-md-4 mb-30">
+                        <div class="single-cta">
+                            <!-- <i class="fas fa-phone"></i> -->
+                            <div class="cta-text">
+                                <h4>Call us</h4>
+                                <span><br/> </span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-4 col-md-4 mb-30">
+                        <div class="single-cta">
+
+                            <!-- <i class="far fa-envelope-open"></i> -->
+                            <div class="cta-text">
+                                <h4>Mail us</h4>
+                                <span>hr@quanteqsolutions.com</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="footer-content pt-5 pb-5">
+                <div class="row">
+                    <div class="col-xl-4 col-lg-4 mb-50">
+                        <div class="footer-widget">
+                            <div class="footer-logo">
+                                <a href="index.php"><img src="images/left-side-logo-quntam.png" class="img-fluid" alt="logo"></a>
+                            </div>
+                            <div class="footer-text" style="padding-right: 20%;">
+                                <p>Trust Quanteq Solutions to be your dependable partner in delivering exceptional consumer care solutions. Let us help you achieve success with our high-quality, customized services.</p>
+                            </div>
+                            <div class="ftco-footer-widget mb-5" >
+                              
+                <h4 class="ftco-heading-2 mb-0" >Connect With Us</h4>
+                <ul class="ftco-footer-social list-unstyled float-md-left float-lft mt-3" style="display:inline-flex;">
+                <!-- <li class="ftco-animate"><a href="#" style="background: rgba(0, 0, 0, 0.1);"><span class="icon-twitter"></span></a></li> -->
+                <li class="ftco-animate"><a href="https://www.facebook.com/profile.php?id=100085491521014"
+ style="background: rgba(0, 0, 0, 0.1);"><span class="icon-facebook"></span></a></li>
+                <li class="ftco-animate"><a href="https://www.instagram.com/quanteq.solutions/" style="background: rgba(0, 0, 0, 0.1);"><span class="icon-instagram"></span></a></li>
+              </ul>
+            </div>
+                            <!-- <div class="footer-social-icon">
+                                <span>Follow us</span>
+
+                                <a href="#"><i class="fab fa-facebook-f facebook-bg"></i></a>
+                                <a href="#"><i class="fab fa-twitter twitter-bg"></i></a>
+                                <a href="#"><i class="fab fa-google-plus-g google-bg"></i></a>
+                            </div> -->
+                        </div>
+                    </div>
+                    <div class="col-xl-4 col-lg-4 col-md-6 mb-30">
+                        <div class="footer-widget">
+                            <div class="footer-widget-heading">
+                                <h3>Useful Links</h3>
+                            </div>
+                            <ul>
+                                
+                                <li><a href="about-us">About Us</a></li>
+                                <!-- <li><a href="services/customer-support.php">Customer Support</a></li> -->
+                                <li><a href="blogs">Blogs</a></li>
+                                <!-- <li><a href="services/lead-generation.php">Lead Generation</a></li> -->
+                                <li><a href="careers">Career</a></li>
+                                <!-- <li><a href="services/digital-marketing.php">Digital Marketing</a></li> -->
+                                
+                                <li><a href="services">Services</a></li>
+                                <!-- <li><a href="services/legal-and-compliance-support.php">Inquiry Handling</a></li> -->
+                                <li><a href="faq">FAQs</a></li>
+                                <!-- <li><a href="services/sales-and-telecommunications.php">TeleMarketing Services</a></li> -->
+                                
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-xl-4 col-lg-4 col-md-6 mb-50">
+                        <div class="footer-widget">
+                            <div class="footer-widget-heading">
+                                <h3>Subscribe</h3>
+                            </div>
+                            <div class="footer-text mb-25">
+                                <p>Don’t miss to subscribe to our new feeds, kindly fill the form below.</p>
+                            </div>
+                            <div class="subscribe-form">
+                <form action="php/subscribe.php" method="post">
+                    <input type="email" name="email" placeholder="Email Address" required>
+                    <button type="submit" style="color:white; font-weight:bold;">Submit</button>
+                </form>
+            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="copyright-area">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xl-6 col-lg-6 text-center text-lg-left">
+                        <div class="copyright-text">
+                            <p> Copyright &copy;<script>document.write(new Date().getFullYear());</script> Quanteq Solution || All Right Reserved </p>
+                        </div>
+                    </div>
+                    <div class="col-xl-6 col-lg-6 d-none d-lg-block text-right">
+                        <div class="footer-menu">
+                            <ul>
+                                <!-- <li><a href="#">Home</a></li> -->
+                                <li><a href="terms-and-condition">Terms & Condition</a></li>
+                                <li><a href="privacy-policy">Privacy Policy</a></li>
+                                <!-- <li><a href="#"></a></li>
+                                <li><a href="#">Contact</a></li> -->
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+  
+
+
+
+   <!--  <footer class="ftco-footer ftco-bg-dark ftco-section">
+      <div class="container">
+        <div class="row mb-5">
+          <div class="col-md-6 col-lg-3">
+            <div class="ftco-footer-widget mb-5">
+                <h2 class="ftco-heading-2">Have a Questions?</h2>
+                <div class="block-23 mb-3">
+                  <ul> -->
+                    <!-- <li><span class="icon icon-map-marker"></span><span class="text">203 Fake St. Mountain View, San Francisco, California, USA</span></li> -->
+                    <!-- <li><a href="#"><span class="icon icon-phone"></span><span class="text">+2 392 3929 210</span></a></li>
+                    <li><a href="#"><span class="icon icon-envelope"></span><span class="text">info@quanteqsolutions.com</span></a></li>
+                  </ul>
+                </div>
+            </div>
+            <div class="ftco-footer-widget mb-5">
+                <h2 class="ftco-heading-2 mb-0">Connect With Us</h2>
+                <ul class="ftco-footer-social list-unstyled float-md-left float-lft mt-3">
+                <li class="ftco-animate"><a href="#"><span class="icon-twitter"></span></a></li>
+                <li class="ftco-animate"><a href="#"><span class="icon-facebook"></span></a></li>
+                <li class="ftco-animate"><a href="#"><span class="icon-instagram"></span></a></li>
+              </ul>
+            </div>
+          </div> -->
+          <!-- <div class="col-md-6 col-lg-3">
+            <div class="ftco-footer-widget mb-5">
+              <h2 class="ftco-heading-2">Recent Blog</h2>
+              <div class="block-21 mb-4 d-flex">
+                <a class="blog-img mr-4" style="background-image: url(images/image_1.jpg);"></a>
+                <div class="text">
+                  <h3 class="heading"><a href="#">Even the all-powerful Pointing has no control about</a></h3>
+                  <div class="meta">
+                    <div><a href="#"><span class="icon-calendar"></span> June 27, 2019</a></div>
+                    <div><a href="#"><span class="icon-person"></span> Admin</a></div>
+                    <div><a href="#"><span class="icon-chat"></span> 19</a></div>
+                  </div>
+                </div>
+              </div>
+              <div class="block-21 mb-5 d-flex">
+                <a class="blog-img mr-4" style="background-image: url(images/image_2.jpg);"></a>
+                <div class="text">
+                  <h3 class="heading"><a href="#">Even the all-powerful Pointing has no control about</a></h3>
+                  <div class="meta">
+                    <div><a href="#"><span class="icon-calendar"></span> June 27, 2019</a></div>
+                    <div><a href="#"><span class="icon-person"></span> Admin</a></div>
+                    <div><a href="#"><span class="icon-chat"></span> 19</a></div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div> -->
+          <!-- <div class="col-md-6 col-lg-3">
+            <div class="ftco-footer-widget mb-5 ml-md-4">
+              <h2 class="ftco-heading-2">Links</h2>
+              <ul class="list-unstyled">
+                <li><a href="#"><span class="ion-ios-arrow-round-forward mr-2"></span>Home</a></li>
+                <li><a href="#"><span class="ion-ios-arrow-round-forward mr-2"></span>About</a></li>
+                <li><a href="#"><span class="ion-ios-arrow-round-forward mr-2"></span>Services</a></li>
+                <li><a href="#"><span class="ion-ios-arrow-round-forward mr-2"></span>Projects</a></li>
+                <li><a href="#"><span class="ion-ios-arrow-round-forward mr-2"></span>Contact</a></li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-md-6 col-lg-3">
+            <div class="ftco-footer-widget mb-5 ml-md-4">
+              <h2 class="ftco-heading-2">Links</h2>
+              <ul class="list-unstyled">
+                <li><a href="#"><span class="ion-ios-arrow-round-forward mr-2"></span>Home</a></li>
+                <li><a href="#"><span class="ion-ios-arrow-round-forward mr-2"></span>About</a></li>
+                <li><a href="#"><span class="ion-ios-arrow-round-forward mr-2"></span>Services</a></li>
+                <li><a href="#"><span class="ion-ios-arrow-round-forward mr-2"></span>Projects</a></li>
+                <li><a href="#"><span class="ion-ios-arrow-round-forward mr-2"></span>Contact</a></li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-md-6 col-lg-3">
+            <div class="ftco-footer-widget mb-5">
+                <h2 class="ftco-heading-2">Subscribe Us!</h2>
+              <form action="#" class="subscribe-form">
+                <div class="form-group">
+                  <input type="text" class="form-control mb-2 text-center" placeholder="Enter email address">
+                  <input type="submit" value="Subscribe" class="form-control submit px-3">
+                </div>
+              </form>
+            </div>
+            
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-12 text-center"> -->
+
+            <!-- <p>
+  Copyright &copy;<script>document.write(new Date().getFullYear());</script>Quanteq Solution || All rights reserved  -->
+  <!-- | This template is made with <i class="icon-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a> -->
+  <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+ <!-- </p>
+          </div>
+        </div>
+      </div>
+    </footer> -->
+
+
+    <script src="js/jquery.min.js"></script>
+  <script src="js/jquery-migrate-3.0.1.min.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/jquery.easing.1.3.js"></script>
+  <script src="js/jquery.waypoints.min.js"></script>
+  <script src="js/jquery.stellar.min.js"></script>
+  <script src="js/owl.carousel.min.js"></script>
+  <script src="js/jquery.magnific-popup.min.js"></script>
+  <script src="js/aos.js"></script>
+  <script src="js/jquery.animateNumber.min.js"></script>
+  <script src="js/scrollax.min.js"></script>
+  <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
+  <script src="js/google-map.js"></script>
+  <script src="js/main.js"></script>
+    
+  </body>
+</html> 

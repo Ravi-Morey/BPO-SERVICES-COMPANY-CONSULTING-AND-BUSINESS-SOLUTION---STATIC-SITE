1b2d3a17bd365470e389686a6eba0fcd43927103
Ravi Morey <124388301+Ravi-Morey@users.noreply.github.com>
2025-06-26 14:29:45 +0530

    add about us file info

---
+++ about-us.php
@@ -0,0 +1,174 @@
+ <?php
+$pageTitle = "About Quanteq Solutions | Empowering Businesses Worldwide";
+$pageDescription = "Quanteq Solutions excels in business communication, finance, and digital marketing services, committed to enhancing your business growth.";
+$pageKeywords = "quanteq , business,services, digital marketing";
+include 'template/header.php';
+?>
+
+<section class="hero-wrap hero-wrap-2" style="background-image: url('images/bg_1.jpg');">
+      <div class="overlay"></div>
+      <div class="container">
+        <div class="row no-gutters slider-text align-items-center justify-content-center">
+          <div class="col-md-9 ftco-animate text-center">
+            <h1 class="mb-2 bread">About Us</h1>
+            <p class="breadcrumbs"><span class="mr-2"><a href="index.php">Home <i class="ion-ios-arrow-forward"></i></a></span> <span>About us <i class="ion-ios-arrow-forward"></i></span></p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="ftco-counter" id="section-counter">
+    	<div class="container">
+    		<div class="row d-md-flex align-items-center justify-content-center">
+    			<div class="wrapper">
+    				<div class="row d-md-flex align-items-center">
+    					<div class="text" style="text-align: center;">
+								<h2 class="mb-2 " style="font-weight: 700;">Our Commitment to Your Success</h2>
+								<p>By choosing Quanteq Solutions, you are partnering with a team dedicated to excellence in consumer care. Our goal is to help your business thrive through our reliable BPO services. We tailor our solutions to meet your specific needs, ensuring that you receive top-notch support that enhances your operations.</p><p>
+Trust Quanteq Solutions to be your dependable partner in delivering exceptional consumer care solutions. Let us help you achieve success with our high-quality, customized services.
+ </p>
+								<!-- <p><a href="#" class="btn btn-primary py-3 px-4">About us </a></p> -->
+							</div>
+    				</div>
+    			</div>
+    		</div>
+    	</div>
+    </section>
+
+    <section class="ftco-section">
+			<div class="container wrap-about ftco-animate">
+				<div class="row justify-content-center mb-0 pb-2 d-flex">
+    			<div class="col-md-6 align-items-stretch d-flex">
+    				<div class="img img-video d-flex align-items-center" style="background-image: url(images/about.jpg);">
+    					<div class="video justify-content-center">
+								<!-- <a href="https://vimeo.com/45830194" class="icon-video popup-vimeo d-flex justify-content-center align-items-center"> -->
+									<span class="ion-ios-play"></span>
+		  					</a>
+							</div>
+    				</div>
+    			</div>
+          <div class="col-md-6 heading-section ftco-animate pl-lg-5 pt-md-0 pt-5">
+            <h2 class="mb-4">We Are the Best Consulting Agency</h2>
+            <p>We provide complete consumer care solutions to our customers throughout the service. Quanteq Solutions Services follow a protocol to always deliver high quality, flawless solutions. Our Staff is highly motivated to satisfy the customer needs and provide 5 stars
+rated <a href="https://quanteqsolutions.com/service/customer-support"><b>customer support</b></a>.
+
+We are known as a renowned BPO possessing good experience in the industry by offering a quality-focused Inbound and Outbound solution with 100% accuracy. Quanteq Solutions
+
+
+Keeps you updated with the latest trends in technology, and helps you deploy the most reliable, secure, and scalable solution.</p>
+            
+          </div>
+        </div>	
+			</div>
+		</section>
+
+    <section class=" bg-light">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6 col-lg-4 ftco-animate">
+            <div class="blog-entry">
+              <a href="#" class="block-20 d-flex align-items-end" style="background-image: url('images/vision1.jpg');">
+                <!-- <div class="meta-date text-center p-2">
+                  <span class="day">26</span>
+                  <span class="mos">June</span>
+                  <span class="yr">2019</span>
+                </div> -->
+              </a>
+              <div class="text bg-white p-4" style="text-align:center;">
+                <h3 class="heading"><a href="#">Quanteq Solutions Vision
+</a></h3>
+                <p>In our vision for the future, businesses flourish in the digital realm through innovative solutions and strategic partnerships. We see ourselves as the driving force behind our clients' success, empowering them to reach and exceed their goals. Setting new benchmarks in<a href="https://quanteqsolutions.com/service/digital-marketing"><b> digital marketing</b></a> and technology, we aim to lead by example. Our unwavering dedication to innovation and customer satisfaction propels us to redefine the boundaries of business achievement in the digital era. Together, we envision a future where every business possesses the tools and know-how to unlock its full potential and leave a meaningful mark on the world.
+
+</p>
+                <!-- <div class="d-flex align-items-center mt-4">
+                  <p class="mb-0"><a href="#" class="btn btn-primary">Read More <span class="ion-ios-arrow-round-forward"></span></a></p>
+                  <p class="ml-auto mb-0">
+                    <a href="#" class="mr-2">Admin</a>
+                    <a href="#" class="meta-chat"><span class="icon-chat"></span> 3</a>
+                  </p>
+                </div> -->
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6 col-lg-4 ftco-animate">
+            <div class="blog-entry">
+              <a href="#" class="block-20 d-flex align-items-end" style="background-image: url('images/mission1.jpg'); ">
+                <!-- <div class="meta-date text-center p-2">
+                  <span class="day">26</span>
+                  <span class="mos">June</span>
+                  <span class="yr">2019</span>
+                </div> -->
+              </a>
+              <div class="text bg-white p-4" style="text-align:center;">
+                <h3 class="heading"><a href="#">Quanteq Solutions Mission</a></h3>
+                <p>Our mission at Quanteq Solution is to provide unparalleled digital solutions that propel businesses towards success. We are committed to delivering innovative strategies and personalized services tailored to the unique needs of each client. Through relentless dedication and expertise, we aim to maximize our clients' online presence, drive growth, and foster long-term relationships. At QuanteqSolution, we strive to be the trusted partner that empowers businesses to thrive in the ever-evolving digital landscape.
+</p>
+                <!-- <div class="d-flex align-items-center mt-4">
+                  <p class="mb-0"><a href="#" class="btn btn-primary">Read More <span class="ion-ios-arrow-round-forward"></span></a></p>
+                  <p class="ml-auto mb-0">
+                    <a href="#" class="mr-2">Admin</a>
+                    <a href="#" class="meta-chat"><span class="icon-chat"></span> 3</a>
+                  </p>
+                </div> -->
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6 col-lg-4 ftco-animate">
+            <div class="blog-entry">
+              <a href="#" class="block-20 d-flex align-items-end" style="background-image: url('images/value1.jpg');">
+                <!-- <div class="meta-date text-center p-2">
+                  <span class="day">26</span>
+                  <span class="mos">June</span>
+                  <span class="yr">2019</span>
+                </div> -->
+              </a>
+              <div class="text bg-white p-4" style="text-align:center;">
+                <h3 class="heading"><a href="#">Quanteq Solutions Values</a></h3>
+                <p>At Quanteq Solution, our core values drive everything we do. We are committed to integrity, ensuring transparency, honesty, and ethical behavior in all our interactions. We prioritize excellence, constantly striving to exceed expectations and deliver exceptional results for our clients. Collaboration is at the heart of our approach, as we believe in working closely with our clients to understand their needs and goals. Innovation is our cornerstone, as we continuously seek out new ideas and technologies to stay ahead of the curve. Finally, we are dedicated to making a positive impact, both in our industry and our community, through responsible business practices and social responsibility initiatives.
+</p>
+                <!-- <div class="d-flex align-items-center mt-4">
+                  <p class="mb-0"><a href="#" class="btn btn-primary">Read More <span class="ion-ios-arrow-round-forward"></span></a></p>
+                  <p class="ml-auto mb-0">
+                    <a href="#" class="mr-2">Admin</a>
+                    <a href="#" class="meta-chat"><span class="icon-chat"></span> 3</a>
+                  </p>
+                </div> -->
+              </div>
+            </div>
+          </div>
+
+          
+        </div>
+        <!-- <div class="row mt-5">
+          <div class="col text-center">
+            <div class="block-27">
+              <ul>
+                <li><a href="#">&lt;</a></li>
+                <li class="active"><span>1</span></li>
+                <li><a href="#">2</a></li>
+                <li><a href="#">3</a></li>
+                <li><a href="#">4</a></li>
+                <li><a href="#">5</a></li>
+                <li><a href="#">&gt;</a></li>
+              </ul>
+            </div>
+          </div>
+        </div> -->
+      </div>
+    </section>
+
+<section class="ftco-section testimony-section " style="background-image: url(images/statergy.jpg); background-repeat: no-repeat; background-size: 100%; ">
+      <div class="container">
+        <div class="row justify-content-center mb-5">
+          <div class="col-md-8 text-center heading-section ftco-animate">
+            <h3 class="mb-4" style="font-weight:700; font-size: 30px;">Business Strategy</h3>
+            <p>At Quanteq Solutions we develop solutions that serve the needs of businesses ready to embrace the future. We synergise with clients to ensure greater potency and constantly balance productivity, quality nad price. We blend the best ideas with world-leading technology to make it eaiser for our patners to connect with their customers. Most of all, we do it all with a smile on our faces as nothing matters more  to us then seeing you succeed. <p>
+
+            	<p style="font-size:18px; font-weight:bold;">It's not what we do but it's what we produce - great customer expriences.</p>
+          </div>
+        </div>
+    </div>
+</section>
+
+
+<?php include('template/footer.php');?>

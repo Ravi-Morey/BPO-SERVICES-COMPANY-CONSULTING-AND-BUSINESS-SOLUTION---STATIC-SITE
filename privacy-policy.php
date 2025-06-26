f40449d350260131a48c84e7f93394a49bf1bb75
Ravi Morey <124388301+Ravi-Morey@users.noreply.github.com>
2025-06-26 15:58:49 +0530

    Required for legal compliance if collecting data

---
+++ privacy-policy.php
@@ -0,0 +1,224 @@
+<?php
+$pageTitle = "Privacy Policy - Quanteq Solutions Pvt Ltd.";
+$pageDescription = "Learn more about Quanteq Solutions Pvt Ltd., our mission, vision, and the team behind our success.";
+$pageKeywords = "Quanteq Solutions, about us, our team, mission, vision";
+include 'template/header.php';
+?>
+
+<section class="hero-wrap hero-wrap-2" style="background-image: url('images/bg_1.jpg');">
+      <div class="overlay"></div>
+      <div class="container">
+        <div class="row no-gutters slider-text align-items-center justify-content-center">
+          <div class="col-md-9 ftco-animate text-center">
+            <h1 class="mb-2 bread">Privacy Policy</h1>
+            <p class="breadcrumbs"><span class="mr-2"><a href="index.php">Home <i class="ion-ios-arrow-forward"></i></a></span> <span>Privacy Policy <i class="ion-ios-arrow-forward"></i></span></p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+
+
+    <section class="ftco-section ftco-animate">
+            <div class="container" >
+                <div class="row d-flex">
+                    
+                    <div class="col-md-12 wrap-about pr-md-4 ftco-animate">
+                        <!-- <div class="img " style="background-image: url(images/image_2.jpg);" ></div> -->
+            <p class="mb-4">We are committed to protecting your privacy and take appropriate measures to maintain the confidentiality and privacy of data shared with us.</p>
+            <h3 style="font-size: 28px;">We Respect Your Privacy
+</h3>
+                        <!-- <p>We offer a range of services designed to address the unique challenges of today’s business landscape: </p> -->
+                        <div class="links">
+                        <ul class="mb-4 pl-3" >
+                                            <!-- list 1 -->
+                                            <li >
+                                                <!-- icon -->
+                                                
+                                                
+                                                <!-- content -->
+                                                <div class="content"> 
+                                                  <!-- <h3>We Respect Your Privacy</h3> -->
+                                                </div>
+                                                
+                                                <p>The information you provide on the website inquiry forms is securely and confidentially stored and processed.
+ </p>
+                                                
+                                            </li>
+                                            
+                                            <!-- list 2 -->
+                                            <li>
+                                                <!-- icon -->
+                                                
+                                                
+                                                <!-- content -->
+                                                <div class="content">
+                                                 <!-- <h3>AI and Automation</h3> -->
+                                                </div>
+                                                
+                                                <p>Your personal details (such as your name, telephone, fax number, and e-mail address), will be used only for communicating with you about our products and services.</p>
+                                                
+                                            </li>
+                                            <li>
+                                                <!-- icon -->
+                                                
+                                                
+                                                <!-- content -->
+                                                <div class="content"> 
+                                                  <!-- <h3>Data Analytics</h3> -->
+                                                </div>
+                                                
+                                                <p>Your personal information will never be shared, leased, sold, or distributed to any third-party entities for marketing purposes without your prior consent.</p>
+                                                
+                                            </li>
+                                            <li>
+                                                <!-- icon -->
+                                                
+                                                
+                                                <!-- content -->
+                                                <div class="content">
+                                                 <!-- <h3>Cloud Services</h3> -->
+                                               </div>
+                                                
+                                                <p> We employ appropriate technologies and processes to prevent unauthorised access, use, and/or disclosure of your personal information.</p>
+                                                
+                                            </li>
+                                             <li>
+                                                <!-- icon -->
+                                                
+                                                
+                                                <!-- content -->
+                                                <div class="content">
+                                                 <!-- <h3>Cloud Services</h3> -->
+                                               </div>
+                                                
+                                                <p> In order to provide you with our products and services we may need to, at times, share your personal information with – </p>
+                                                <ol>
+                                                  <li>Business partners</li>
+                                                  <li>Affiliates</li>
+                                                  <li>Authorised service vendors and contractors</li>
+                                                </ol>
+                                                
+                                            </li>
+
+                                            
+                                        </ul>
+                                    </div>
+                        
+                        <h3 style="font-size: 28px;">Your Information is Leveraged to Serve You Better (Cookies)</h3>
+                        
+                        <div class="links">
+                        <ul class="mb-4 pl-3" >
+                                            <!-- list 1 -->
+                                            <li >
+                                                <!-- icon -->
+                                                
+                                                
+                                                <!-- content -->
+                                                <!-- <div class="content"> <h3>User-Centered Design</h3></div> -->
+                                                
+                                                <p>Cookies are used on our websites to provide you with more personalised and effective user experience.</p>
+                                                
+                                            </li>
+                                            
+                                            <!-- list 2 -->
+                                            <li>
+                                                <!-- icon -->
+                                                
+                                                
+                                                <!-- content -->
+                                                <!-- <div class="content"> <h3>Customer Support</h3></div> -->
+                                                
+                                                <p> These cookies do not grant us remote access to your systems or to any confidential information you have not shared with us.</p>
+                                                
+                                            </li>
+                                            <li>
+                                               
+                                                
+                                                <p> We will never make any data related to your location or any other customer-centric information public. </p>
+                                                
+                                            </li>
+                                            <li>
+                                               
+                                                
+                                                <p>Cookies are stored and restricted to your browser or system memory. They can be disabled by changing your browser’s settings. But turning off cookies may prevent you from obtaining the full benefits of using our website. </p>
+                                                
+                                            </li>
+                                            <li>
+                                               
+                                                
+                                                <p> To understand more about cookies and how they are used, visit allaboutcookies.org. </p>
+                                                
+                                            </li>
+
+                                            
+
+                                            
+                                        </ul>
+                                    </div>
+
+                                     <h3 style="font-size: 28px;">Links to External Sites</h3>
+                        <!-- <p>By integrating cutting-edge technology with a focus on human experience, Quanteq Solutions helps businesses achieve: </p> -->
+                        <div class="links">
+                        <ul class="mb-4 pl-3" >
+                                            <!-- list 1 -->
+                                            <li >
+                                                
+                                                <p>Any links on our website to external third-party sites are for your convenience only and should be accessed at your own risk.</p>
+                                                
+                                            </li>
+                                            
+                                            <!-- list 2 -->
+                                            <li>
+                                                
+                                                <p>Quanteq Solutions is not responsible in any manner for the security features, content, or privacy policies of these third-party websites.</p>
+                                                
+                                            </li>
+                                            
+                                            
+
+                                            
+                                        </ul>
+
+                                        <h3 style="font-size: 28px;">GDPR Compliance (For Visitors to our Website from the
+European Union)</h3>
+                        <!-- <p>By integrating cutting-edge technology with a focus on human experience, Quanteq Solutions helps businesses achieve: </p> -->
+                        <div class="links">
+                        <ul class="mb-4 pl-3" >
+                                            <!-- list 1 -->
+                                            <li >
+                                                
+                                                <p>Basic identification details provided by you on our websites such as name, email address, company name, telephone number, and country, as well as web data like IP address, system location, and cookies are securely and confidentially stored.</p>
+                                                
+                                            </li>
+                                            
+                                            <!-- list 2 -->
+                                            <li>
+                                                
+                                                <p>This information will be used only for communication with clients and potential customers about our products and services.</p>
+                                                
+                                            </li>
+                                            <li>
+                                                
+                                                <p>We will not retain this personal information any longer than necessary to achieve the purposes defined in this privacy policy unless a longer retention period is specified by the law or for directly related legitimate business purposes.</p>
+                                                
+                                            </li>
+                                            <li>
+                                                
+                                                <p>You may request us to provide copies of your personal information, how and where the data is stored, and the purpose of storing this data.</p>
+                                                
+                                            </li>
+                                            
+                                            
+
+                                            
+                                        </ul>
+                                        <h3 style="font-size: 28px;">Changes to Our Privacy Policy</h3>
+                        <p>Quanteq Solutions will review, change or update our privacy policy as and when required. We will ensure that our updated privacy policy is made available on our website </p> 
+                                    </div>
+                                  </div>
+                                </div>
+                              </div>
+                            </section>
+
+    <?php include('template/footer.php') ?>

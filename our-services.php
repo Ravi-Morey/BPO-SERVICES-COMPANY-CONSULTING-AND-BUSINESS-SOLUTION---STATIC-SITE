e905df92f7df5b6722c28052d001a533e9a98c23
Ravi Morey <124388301+Ravi-Morey@users.noreply.github.com>
2025-06-26 15:56:12 +0530

    Useful for client conversion

---
+++ our-services.php
@@ -0,0 +1,277 @@
+<?php
+$pageTitle = "Quanteq Solutions: Business communication, accounting, finance, digital marketing, customer support, and SEO.";
+$pageDescription = "Quanteq Solutions offers expertise in business communication, accounting, finance, 
+digital marketing, and customer support to elevate your business success.";
+$pageKeywords = "solutions, business, services, quanteq, support, customer, marketing, digital, finance
+";
+include 'template/header.php';
+?>
+<section class="hero-wrap hero-wrap-2" style="background-image: url('images/bg_1.jpg');">
+      <div class="overlay"></div>
+      <div class="container">
+        <div class="row no-gutters slider-text align-items-center justify-content-center">
+          <div class="col-md-9 ftco-animate text-center">
+            <h1 class="mb-2 bread">Services</h1>
+            <p class="breadcrumbs"><span class="mr-2"><a href="index.php">Home <i class="ion-ios-arrow-forward"></i></a></span> <span>Services <i class="ion-ios-arrow-forward"></i></span></p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="ftco-section ftco-no-pb" >
+    	<div class="container">
+				<div class="row d-flex">
+    	
+    	<div class="row mt-6">
+							<div class="col-lg-4">
+								<div class="services  text-center">
+									<div class="icon mt-2 d-flex justify-content-center align-items-center"><span class="flaticon-collaboration"></span></div>
+									<div class="text media-body">
+										<h3>Business Communications</h3>
+										<p>Elevate your company's interactions with our expert Business Communications Service, offering streamlined, professional solutions for seamless and effective internal and external.
+</p>
+										<p><a href="service/business-communications" class="btn btn-primary py-3 px-4">Read more </a></p>
+									</div>
+								</div>
+								<div class="services text-center">
+									<div class="icon mt-2 d-flex justify-content-center align-items-center"><span class="flaticon-business"></span></div>
+									<div class="text media-body">
+										<h3>Customer Support</h3>
+										<p>Quanteq Solutions is providing 24/7 personalized assistance with fast, friendly solutions. We ensure seamless experiences, resolving issues efficiently to exceed customer expectations.</p>
+										<p><a href="service/customer-support" class="btn btn-primary py-3 px-4">Read more </a></p>
+									</div>
+								</div>
+								<div class="services  text-center">
+									<div class="icon mt-2 d-flex justify-content-center align-items-center"><span class="icon-database"></span></div>
+									<div class="text media-body">
+										<h3>Lead Generation</h3>
+										<p>In the fast-paced, ever-evolving landscape of modern business, identifying and connecting with potential customers is crucial for sustained growth.
+</p>
+										<p><a href="service/lead-generation.php" class="btn btn-primary py-3 px-4">Read more</a></p>
+									</div>
+								</div>
+								
+							</div>
+							<div class="col-lg-4">
+								<div class="services  text-center">
+									<div class="icon mt-2 d-flex justify-content-center align-items-center"><span class="flaticon-collaboration"></span></div>
+									<div class="text media-body">
+										<h3>Accounting & Finance</h3>
+										<p>Acquire expert accounting and finance services, ensuring precise bookkeeping, financial analysis, and strategic planning for optimized business growth and compliance.
+</p>
+										<p><a href="service/accounting-and-finance" class="btn btn-primary py-3 px-4">Read more</a></p>
+									</div>
+								</div>
+								<div class="services text-center">
+									<div class="icon mt-2 d-flex justify-content-center align-items-center"><span class="flaticon-analysis"></span></div>
+									<div class="text media-body">
+										<h3>Digital Marketing</h3>
+										<p>Maximize your online presence with our comprehensive digital marketing services: SEO, PPC, social media, and content marketing tailored to boost your business growth.</p>
+										<p><a href="service/digital-marketing" class="btn btn-primary py-3 px-4">Read more</a></p>
+									</div>
+								</div>
+								<div class="services text-center">
+									<div class="icon mt-2 d-flex justify-content-center align-items-center"><span class="flaticon-search-engine"></span></div>
+									<div class="text media-body">
+										<h3>Website Development</h3>
+										<p>QuanteqSolutions delivers world-class web development services, tailored to boost traffic and enhance your business's online visibility.</p>
+										<p><a href="service/website-development" class="btn btn-primary py-3 px-4">Read more</a></p>
+									</div>
+								</div>
+							</div>
+							<div class="col-lg-4">
+								<div class="services text-center">
+									<div class="icon mt-2 d-flex justify-content-center align-items-center"><span class="flaticon-search-engine"></span></div>
+									<div class="text media-body">
+										<h3>Legal & Compliance Support</h3>
+										<p>Expert legal and compliance support, ensuring your business adheres to regulations, mitigates risks, and maintains ethical standards across all operations and jurisdictions.</p>
+										<p><a href="service/legal-and-compliance-support" class="btn btn-primary py-3 px-4">Read more</a></p>
+									</div>
+								</div>
+								<div class="services text-center">
+									<div class="icon mt-2 d-flex justify-content-center align-items-center"><span class="flaticon-handshake"></span></div>
+									<div class="text media-body">
+										<h3>Sales & Telecommunications</h3>
+										<p>Quanteq Solutions is offering seamless connectivity, innovative solutions, and tailored support to enhance your sales and business communications to drive growth.</p>
+										<p><a href="service/sales-and-telecommunications" class="btn btn-primary py-3 px-4">Read more</a></p>
+									</div>
+								</div>
+								<!-- <div class="services text-center">
+									<div class="icon mt-2 d-flex justify-content-center align-items-center"><span class="flaticon-handshake"></span></div>
+									<div class="text media-body">
+										<h3>Sales & Telecommunications</h3>
+										<p>Quanteq Solutions is offering seamless connectivity, innovative solutions, and tailored support to enhance your sales and business communications to drive growth.</p>
+										<p><a href="service/sales-and-telecommunications" class="btn btn-primary py-3 px-4">Read more</a></p>
+									</div>
+								</div> -->
+							</div>
+							</div>
+							
+							</div>
+				</div>
+							
+
+							
+
+						</div>
+					</div>
+				</div>
+    </section>
+
+    <section class="ftco-section">
+    	<div class="container">
+    		<!-- <div class="row justify-content-center mb-5">
+          <div class="col-md-8 text-center heading-section ftco-animate">
+          	<span class="subheading">Services</span>
+            <h2 class="mb-4">Why Choose Us?</h2>
+            <p>Separated they live in. A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country</p>
+          </div>
+        </div> style="background-color: aliceblue;" -->
+  			<div class="row tabulation mt-4 ftco-animate">
+  				<div class="col-md-4" >
+						<ul class="nav nav-pills nav-fill d-md-flex d-block flex-column">
+						  <li class="nav-item text-left">
+						    <a class="nav-link active py-4" data-toggle="tab" href="#services-1"><span class="flaticon-analysis mr-2"></span> We Build Lasting Values</a>
+						  </li>
+						  <li class="nav-item text-left">
+						    <a class="nav-link py-4" data-toggle="tab" href="#services-2"><span class="flaticon-business mr-2"></span> Increase Qulaity & Efficiency</a>
+						  </li>
+						  <li class="nav-item text-left">
+						    <a class="nav-link py-4" data-toggle="tab" href="#services-3"><span class="flaticon-insurance mr-2"></span> We Build Lasting Values</a>
+						  </li>
+						  <!-- <li class="nav-item text-left">
+						    <a class="nav-link py-4" data-toggle="tab" href="#services-4"><span class="flaticon-money mr-2"></span> Global Investigation</a>
+						  </li>
+						  <li class="nav-item text-left">
+						    <a class="nav-link py-4" data-toggle="tab" href="#services-5"><span class="flaticon-rating mr-2"></span> Audit &amp; Evaluation</a>
+						  </li>
+						  <li class="nav-item text-left">
+						    <a class="nav-link py-4" data-toggle="tab" href="#services-6"><span class="flaticon-search-engine mr-2"></span> Marketing Strategy</a>
+						  </li> -->
+						</ul>
+					</div>
+					<div class="col-md-8">
+						<div class="tab-content">
+						  <div class="tab-pane container p-0 active" id="services-1">
+						  	<div class="img" style="background-image: url(images/project-2.jpg);"></div>
+						  	<h3><a href="#">We Build Lasting Values</a></h3>
+						  	<p>We help our clients find perfect fit and build dedicated teams to lower costs and create long term value for their organisation.</p><p >
+Quanteq Solutions is Next generation driven. that means we put human intelligence and passion first. We Simplify rather then complicate. We deploy the power of technological magic. Most of all, we offer solutions that deliver memorable customer experiences and, in turn, better outcomes for our clients.</p><ul><p class="mr-2">
+	<li>Sales & Telecommunications</li>
+	<li>Accounting & Finance</li>
+	<li>Customer Support</li>
+	<li>Digital Marketing</li></p>
+</ul>
+						  </div>
+						  <div class="tab-pane container p-0 fade" id="services-2">
+						  	<div class="img" style="background-image: url(images/project-3.jpg);"></div>
+						  	<h3><a href="#">Increase Qulaity & Efficiency </a></h3>
+						  	<p>Our world class staffing solutions lead to exceptional internal processes, access to experienced BPO consultants and a global talent pool.</p>
+						  	<p>Our world class staffing solutions lead to exceptional internal processes, access to experienced BPO consultants and a global talent pool.</p>
+						  	<p>Being different also makes it easier for us help our client navigate the constant changes happening around them. We're not different for the sake of being different - we're different because it's the key to achieving great customer experience.</p>
+						  </div>
+						  <div class="tab-pane container p-0 fade" id="services-3">
+						  	<div class="img" style="background-image: url(images/project-4.jpg);"></div>
+						  	<h3><a href="#">We Build Lasting Values</a></h3>
+						  	<p>Setup your offshore operations with outsourced staff you require to help you scale quickly to meet demand, without all the overheads. </p>
+						  	<p>We are a trusted global solutions organization. We are one of the fastest-growing enterprises in the outsourcing world featuring as an upcoming leader and a business partner.</p>
+						  	<p>Our diverse focus on each aspect of the business makes us unique and innovative. The ability to integrate various businesses makes it a one-stop solution for businesses.</p>
+						  </div>
+						 <!--  <div class="tab-pane container p-0 fade" id="services-4">
+						  	<div class="img" style="background-image: url(images/project-5.jpg);"></div>
+						  	<h3><a href="#">Global Investigation</a></h3>
+						  	<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
+						  </div>
+						  <div class="tab-pane container p-0 fade" id="services-5">
+						  	<div class="img" style="background-image: url(images/project-6.jpg);"></div>
+						  	<h3><a href="#">Audit &amp; Evaluation</a></h3>
+						  	<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
+						  </div>
+						  <div class="tab-pane container p-0 fade" id="services-6">
+						  	<div class="img" style="background-image: url(images/project-1.jpg);"></div>
+						  	<h3><a href="#">Marketing Strategy</a></h3>
+						  	<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separ-gvated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
+						  </div> -->
+						</div>
+					</div>
+				</div>
+    	</div>
+    </section>
+
+    <section class="ftco-section ftco-about ftco-no-pt ftco-no-pb ftco-counter" id="section-counter" style="background-color: palegoldenrod; padding-top: 5em !important;">
+			<div class="container consult-wrap ">
+				<div class="row d-flex align-items-stretch">
+					<div class="col-md-6 wrap-about align-items-stretch d-flex">
+						<div class="heading-section mb-4 ">
+							<!-- <span class="subheading " >Welcome to Negotiate</span> -->
+							<h2>
+Offering a quality-focused inbound and outbound solution with 100% accuracy.</h2>
+						
+						<p>quanteq soltuions keeps you updated with the latest treads in technology, and helps ou deploy the most reliable, secure, and scalable solution.</p></div>
+						<!-- <div class="img" style="background-image: url(images/about.jpg);"></div> -->
+					</div>
+					<div class="col-md-6 wrap-about ftco-animate py-md-5 pl-md-5">
+						
+						<!-- <div class="tabulation-2 mt-4">
+							<ul class="nav nav-pills nav-fill d-md-flex d-block">
+							  <li class="nav-item">
+							    <a class="nav-link active py-2" data-toggle="tab" href="#home1"><span class="ion-ios-home mr-2"></span> Our Mission</a>
+							  </li>
+							  <li class="nav-item px-lg-2">
+							    <a class="nav-link py-2" data-toggle="tab" href="#home2"><span class="ion-ios-person mr-2"></span> Our Vision</a>
+							  </li>
+							  <li class="nav-item">
+							    <a class="nav-link py-2" data-toggle="tab" href="#home3"><span class="ion-ios-mail mr-2"></span> Our Value</a>
+							  </li>
+							</ul>
+							<div class="tab-content bg-light rounded mt-2">
+							  <div class="tab-pane container p-0 active" id="home1">
+							  	<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
+							  </div>
+							  <div class="tab-pane container p-0 fade" id="home2">
+							  	<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
+							  </div>
+							  <div class="tab-pane container p-0 fade" id="home3">
+							  	<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
+							  </div>
+							</div>
+						</div> -->
+    				<div class="row mt-3">
+		          <div class="col-md d-flex justify-content-center counter-wrap ftco-animate">
+		            <div class="block-18" style="background: repeating-radial-gradient(white, transparent 100px);
+    padding: 10px;">
+		            	<div class="icon"><span class="flaticon-doctor"></span></div>
+		              <div class="text">
+		                <strong class="number" data-number="1387">0</strong>
+		                <span>Happy Clients</span>
+		              </div>
+		            </div>
+		          </div>
+		          <div class="col-md d-flex justify-content-center counter-wrap ftco-animate">
+		            <div class="block-18" style="background: repeating-radial-gradient(white, transparent 100px);
+    padding: 10px;">
+		            	<div class="icon"><span class="flaticon-doctor"></span></div>
+		              <div class="text">
+		                <strong class="number" data-number="310">0</strong>
+		                <span>Global Market</span>
+		              </div>
+		            </div>
+		          </div>
+		          <div class="col-md d-flex justify-content-center counter-wrap ftco-animate">
+		            <div class="block-18" style="background: repeating-radial-gradient(white, transparent 100px);
+    padding: 10px;">
+		            	<div class="icon"><span class="flaticon-doctor"></span></div>
+		              <div class="text">
+		                <strong class="number" data-number="3">0</strong>
+		                <span>Experienced</span>
+		              </div>
+		            </div>
+		          </div>
+	          </div>
+					</div>
+				</div>
+			</div>
+		</section>
+
+
+<?php include('template/footer.php'); ?>
